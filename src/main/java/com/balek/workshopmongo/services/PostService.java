aeb859bebaa16b76ce31f7a479c83f56a965f59d
BarBeerCue <leonardobalek@gmail.com>
2024-03-18 13:34:40 +0100

    obtendo poost por id

---
+++ src/main/java/com/balek/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.balek.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.balek.workshopmongo.domain.Post;
+import com.balek.workshopmongo.repository.PostRepository;
+import com.balek.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}	
+}
