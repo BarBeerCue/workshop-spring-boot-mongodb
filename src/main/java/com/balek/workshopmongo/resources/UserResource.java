7ebdee328a136ce87836463562f41d5aaedbdf51
BarBeerCue <leonardobalek@gmail.com>
2024-03-18 13:28:32 +0100

    endpoint para retornar os posts de um usuario

--- src/main/java/com/balek/workshopmongo/resources/UserResource.java
+++ src/main/java/com/balek/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.balek.workshopmongo.domain.Post;
 import com.balek.workshopmongo.domain.User;
 import com.balek.workshopmongo.dto.UserDTO;
 import com.balek.workshopmongo.services.UserService;
@@ -57,5 +58,10 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>>  findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
